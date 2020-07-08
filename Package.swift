5a99cbbd99c554f775633f4103c3527362ff4389
Bas van der Hoek <bas@hoek.com>
2020-07-08 22:28:04 +0200

    macOS 10.11 https://medium.com/@joesusnick/swift-package-manager-with-a-mixed-swift-and-objective-c-project-part-2-2-e71dad234e6

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyDropbox",
     platforms: [
-        .macOS(.v10_14), .iOS(.v9)
+        .macOS(.v10_11), .iOS(.v9)
     ],
     products: [.library(name: "SwiftyDropbox", targets: ["SwiftyDropbox"])],
     dependencies: [
