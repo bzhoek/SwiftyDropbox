d3b4b9fd06ae3244b08a758675d42ac5c43af7ea
Bas van der Hoek <bas@hoek.com>
2020-07-08 19:39:02 +0200

    products

--- Package.swift
+++ Package.swift
@@ -7,7 +7,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyDropbox",
+    products: [.library(name: "SwiftyDropbox", targets: ["SwiftyDropbox"])],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "4.8.2")),
+    ],
+    targets: [
+    .target(
+        name: "SwiftyDropbox",
+        dependencies: ["Alamofire"]),
     ]
 )
